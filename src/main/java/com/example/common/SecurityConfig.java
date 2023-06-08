c5450e3a56654b6acd674f1b37bcc0d3dc09e6e6
igamasayuki <igamasayuki@gmail.com>
2023-06-08 13:38:50 +0900

    SpringBootのバージョン最新化

--- src/main/java/com/example/common/SecurityConfig.java
+++ src/main/java/com/example/common/SecurityConfig.java
@@ -45,24 +45,23 @@ public class SecurityConfig {
 				// /admin/から始まるパスはADMIN権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
 				// .requestMatchers("/user/**").hasRole("USER") //
 				// /user/から始まるパスはUSER権限でログインしている場合のみアクセス可(権限設定時の「ROLE_」を除いた文字列を指定)
-				.anyRequest().authenticated(); // それ以外のパスは認証が必要
+				.anyRequest().authenticated();
+		
+        http.formLogin(login -> login // ログインに関する設定
+                .loginPage("/") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
+                .loginProcessingUrl("/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
+                .failureUrl("/?error=true") // ログイン失敗に遷移させるパス
+                .defaultSuccessUrl("/employee/showList", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
+                // 第2引数: true :認証後常に第1引数のパスに遷移
+                // false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
+                .usernameParameter("mailAddress") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
+                .passwordParameter("password")); // 認証時に使用するパスワードのリクエストパラメータ名
 
-		http.formLogin() // ログインに関する設定
-				.loginPage("/") // ログイン画面に遷移させるパス(ログイン認証が必要なパスを指定してかつログインされていないとこのパスに遷移される)
-				.loginProcessingUrl("/login") // ログインボタンを押した際に遷移させるパス(ここに遷移させれば自動的にログインが行われる)
-				.failureUrl("/?error=true") // ログイン失敗に遷移させるパス
-				.defaultSuccessUrl("/employee/showList", false) // 第1引数:デフォルトでログイン成功時に遷移させるパス
-				// 第2引数: true :認証後常に第1引数のパスに遷移
-				// false:認証されてなくて一度ログイン画面に飛ばされてもログインしたら指定したURLに遷移
-				.usernameParameter("mailAddress") // 認証時に使用するユーザ名のリクエストパラメータ名(今回はメールアドレスを使用)
-				.passwordParameter("password"); // 認証時に使用するパスワードのリクエストパラメータ名
-
-		http.logout() // ログアウトに関する設定
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout**")) // ログアウトさせる際に遷移させるパス
-				.logoutSuccessUrl("/") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
-				.deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
-				.invalidateHttpSession(true); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
-
+        http.logout(logout -> logout // ログアウトに関する設定
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout**")) // ログアウトさせる際に遷移させるパス
+                .logoutSuccessUrl("/") // ログアウト後に遷移させるパス(ここではログイン画面を設定)
+                .deleteCookies("JSESSIONID") // ログアウト後、Cookieに保存されているセッションIDを削除
+                .invalidateHttpSession(true)); // true:ログアウト後、セッションを無効にする false:セッションを無効にしない
 		return http.build();
 	}
 
