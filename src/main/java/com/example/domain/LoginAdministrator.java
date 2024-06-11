40d01ef7df6b68ef1702cf6d37cf7aadc249ff40
igamasayuki <igamasayuki@gmail.com>
2024-06-11 13:18:42 +0900

    不要な行の削除など

--- src/main/java/com/example/domain/LoginAdministrator.java
+++ src/main/java/com/example/domain/LoginAdministrator.java
@@ -13,12 +13,11 @@ import org.springframework.security.core.userdetails.User;
  */
 public class LoginAdministrator  extends User{
 
-	private static final long serialVersionUID = 1L;
 	/** 管理者情報 */
 	private final Administrator administrator;
 	
 	/**
-	 * 通常の管理者情報に加え、認可用ロールを設定する。
+	 * 通常の管理者情報に加え、認可用ロールを設定する.
 	 * 
 	 * @param administrator 管理者情報
 	 * @param authorityList 権限情報が入ったリスト
